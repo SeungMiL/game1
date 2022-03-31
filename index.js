4f564954eb415cce008b9c1d6044f8cf5ad05c4d
SeungMiL <tmskqj@naver.com>
2022-04-01 01:47:42 +0900

    1

---
+++ index.js
@@ -0,0 +1,106 @@
+const canvas = document.querySelector("canvas");
+
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const gravity = 1.5;
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 30;
+    this.height = 30;
+  }
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update() {
+    this.draw();
+
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height)
+      this.velocity.y += gravity;
+    else this.velocity.y = 0;
+  }
+}
+
+const player = new Player();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  if(keys.right.pressed){
+      player.velocity.x = 5
+  } else if (keys.left.pressed){
+      player.velocity.x = -5
+  } else player.velocity.x =0
+}
+
+animate();
+
+addEventListener("keydown", ({ keyCode }) => {
+  console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("왼쪽");
+      keys.left.pressed = true;
+
+      break;
+    case 83:
+      console.log("아래");
+      break;
+    case 68:
+      console.log("오른쪽");
+      keys.right.pressed = true;
+      break;
+    case 87:
+      console.log("위");
+      player.velocity.y -= 20;
+      break;
+  }
+});
+
+addEventListener("keyup", ({ keyCode }) => {
+  console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("왼쪽");
+      keys.left.pressed = false;
+
+      break;
+    case 83:
+      console.log("아래");
+      break;
+    case 68:
+      console.log("오른쪽");
+      keys.right.pressed = false;
+
+      break;
+    case 87:
+      console.log("위");
+      player.velocity.y -= 20;
+      break;
+  }
+});
